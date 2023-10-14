b94fafd09ff81534e2d87a8cc4b7e5cef494bf0f
H5GG3x <140417382+H5GG3x@users.noreply.github.com>
2023-10-14 23:43:59 +0800

    Add files via upload

---
+++ 错误页面.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error Page</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: Arial, sans-serif;
+        }
+        .error-container {
+            text-align: center;
+        }
+        .error-container h1 {
+            font-size: 3em;
+            color: red;
+        }
+        .error-container p {
+            font-size: 1.2em;
+        }
+    </style>
+</head>
+<body>
+    <div class="error-container">
+        <h1>Error</h1>
+        <p>您的ip地址错误</p>
+    </div>
+</body>
+</html>
